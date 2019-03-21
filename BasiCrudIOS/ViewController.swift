9fd76525e9749cf101571fa5c3d00634278734d7
miguelcecci <miguel.cece@hubchain.io>
2019-03-21 17:27:50 -0300

    Initial Commit

---
+++ BasiCrudIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasiCrudIOS
+//
+//  Created by Miguel Angelo Cecci de Castro Neto on 21/03/19.
+//  Copyright © 2019 Miguel Angelo Cecci de Castro Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BasiCrudIOSTests/BasiCrudIOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  BasiCrudIOSTests.swift
+//  BasiCrudIOSTests
+//
+//  Created by Miguel Angelo Cecci de Castro Neto on 21/03/19.
+//  Copyright © 2019 Miguel Angelo Cecci de Castro Neto. All rights reserved.
+//
+
+import XCTest
+@testable import BasiCrudIOS
+
+class BasiCrudIOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
